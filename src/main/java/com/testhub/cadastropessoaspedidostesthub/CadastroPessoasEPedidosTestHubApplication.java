1e749298fd68fdd0a4063e95c65f729c7ac31b29
Humberto Guerrer <humberto.guerrer@gmail.com>
2025-08-13 15:09:04 -0300

    Iniciando a criacao da API para o projeto TestHub

---
+++ src/main/java/com/testhub/cadastropessoaspedidostesthub/CadastroPessoasEPedidosTestHubApplication.java
@@ -0,0 +1,13 @@
+package com.testhub.cadastropessoaspedidostesthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroPessoasEPedidosTestHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroPessoasEPedidosTestHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testhub/cadastropessoaspedidostesthub/CadastroPessoasEPedidosTestHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testhub.cadastropessoaspedidostesthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPessoasEPedidosTestHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
