89febaf22557aa49016e79d85d8216a8acba0052
Humberto Guerrer <humberto.guerrer@gmail.com>
2025-08-15 01:42:51 -0300

    Adicionando configuracao de CORS

---
+++ src/main/java/com/testhub/cadastropessoaspedidostesthub/config/ConfiguracaoCors.java
@@ -0,0 +1,18 @@
+package com.testhub.cadastropessoaspedidostesthub.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfiguracaoCors implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry cors) {
+        cors.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
